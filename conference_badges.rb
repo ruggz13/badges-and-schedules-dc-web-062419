ce7059de5aa72276ea41cbdbea0adfb9e4b4d20e
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-14 20:27:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees == "Hello, my name is Johnny")
+def batch_badge_creator(attendees = "Hello, my name is Johnny")
   return badges
 end
