27defb52a693e1f68393da715cf6714018bc8918
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-14 20:40:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,10 @@ def batch_badge_creator(attendees)
   return attendees.map{|badges| badge_maker(badges)}
 end
 
-def assign_rooms(attendees)
-
+def assign_rooms(names)
+  rooms = []
+  name.each.with_index(1) do |name, room_assignment|
+    rooms << room_assignment = "Hello, #{name}! You'll be assigned to room #{room_assignment}"
+  end
+  return rooms
 end
